558a7f35ee972cedb050fa2d8c52bad69f5898c2
javie <javie@LAPTOP-GGUJC36B>
2023-09-22 09:42:45 -0600

    Cambios en el View

--- MVCLAB0/View/main.py
+++ MVCLAB0/View/main.py
@@ -1,54 +1,122 @@
 
-from venv.Model.ArbolB import ArbolB
+from Model.ArbolB import ArbolB
+from Model.Persona import Persona
+from Model.NodoB import NodoB
+from Controller.concatenacion import concatenacion
+from Controller.HuffmanTree import HuffmanTree
 
+import json
 print("---------------------------------------------------------")
 print("---- Bienvenido al Sistema de Busqueda de Candidatos ----")
 
 salir = False
 
-arbolB = ArbolB(3)  # Por ejemplo, t=3
+arbolB = ArbolB(500)  # Por ejemplo, t=3/
 
 while not salir:
     print("---------------------------------------------------------")
     print("1. Insertar Archivo JSON")
-    print("2. Busqueda de Candidatos")
-    print("3. Salir")
+    print("2. Busqueda de Candidatos por Nombre")
+    print("3. Busqueda de Candidatos por DPI")
+    print("4. Salir")
     print("---------------------------------------------------------")
     opc = input("Seleccione una opción: ")
 
     if opc == "1":
+        print("-----------------------------------------------------------------------")
         # Llamar al código para insertar el archivo JSON en el árbol B
         if arbolB is not None:
             try:
                 # Leer el archivo JSON
-                with open("Users\javie\PycharmProjects\MVCLAB0\venv\View\texto.txt", "r") as file:
+                with open("texto.txt", "r") as file:
                     data = file.readlines()
 
                     for linea in data:
-                        # Analizar el JSON
-                        json_data = json.loads(linea)
+                        # Separar la línea en partes usando el punto y coma como separador
+                        partes = linea.strip().split(';')
 
-                        # Realizar operaciones según el tipo (INSERT, PATCH, DELETE)
-                        tipo_operacion = json_data["tipo"]
-                        data = json_data["data"]
+                        # Verificar que haya al menos dos partes (tipo de operación y datos)
+                        if len(partes) >= 2:
+                            tipo_operacion = partes[0].strip()
+                            datos_str = partes[1].strip()
 
-                        if tipo_operacion == "INSERT":
-                            # Crear una persona a partir de los datos del JSON
-                            persona = Model.Persona()
-                            persona.set_nombre(data["name"])
-                            persona.set_dpi(data["dpi"])
-                            persona.set_fecha(data["dateBirth"])
-                            persona.set_direccion(data["address"])
+                            if tipo_operacion == "INSERT":
+                                # Analizar los datos JSON
+                                try:
+                                    # Analizar los datos JSON
+                                    data = json.loads(datos_str)
 
-                            # Insertar la persona en el árbol B
-                            arbolB.insertar(persona)
-                        elif tipo_operacion == "PATCH":
-                            # Implementa la lógica para actualizar una persona en el árbol B
-                            pass
-                        elif tipo_operacion == "DELETE":
-                            # Implementa la lógica para eliminar una persona del árbol B
-                            pass
+                                    # Crear una persona a partir de los datos del JSON
+                                    # Json dumps hacerlo a la inversa, genera un string json
+                                    # Mandar la informacion directamente al objeto de la persona
 
+                                    persona = Persona()
+                                    persona.set_nombre(data["name"])
+                                    persona.set_dpi(data["dpi"])
+                                    persona.set_fecha(data["datebirth"])
+                                    persona.set_direccion(data["address"])
+
+                                    companies = []
+
+                                    conc = concatenacion();
+                                    companies = conc.process_companies(data["dpi"],data["companies"])
+                                    persona.set_compa(companies)
+                                    # Crear una instancia de HuffmanTree para cada cadena de texto
+                                    huf_trees = [HuffmanTree(companies[i]) for i in range(len(companies))]
+
+                                    # Codificar y decodificar cada cadena usando las instancias correspondientes
+                                    encriptados = []
+                                    desencriptados = []
+                                    persona.set_des(desencriptados)
+
+                                    for i in range(len(companies)):
+                                        binary_output = huf_trees[i].text_to_binary_huffman(companies[i])
+                                        encriptados.append(binary_output)
+
+                                    # Insertar la persona en el árbol B
+                                    persona.set_enc(encriptados)
+                                    arbolB.insertar(persona)
+
+                                except json.JSONDecodeError as json_error:
+                                    print(f"Error al decodificar JSON en línea: {linea}")
+                                    print(f"Mensaje de error: {json_error}")
+                                except Exception as e:
+                                    print(f"Error inesperado: {e}")
+                            elif tipo_operacion == "PATCH":
+                                try:
+                                    data = json.loads(datos_str)
+
+                                    name = data.get("name")
+                                    dpi = data.get("dpi")
+                                    fecha = data.get("datebirth")
+                                    address = data.get("address")
+                                    if address is None or address == "":
+                                        arbolB.actualizarDate(name,dpi,fecha)
+                                        pass
+                                    else:
+                                        arbolB.actualizarAdress(name, dpi, address)
+                                        pass
+                                except json.JSONDecodeError as json_error:
+                                    print(f"Error al decodificar JSON en línea: {linea}")
+                                    print(f"Mensaje de error: {json_error}")
+                                except Exception as e:
+                                    print(f"Error inesperado: {e}")
+
+                                pass
+                            elif tipo_operacion == "DELETE":
+                                try:
+                                    data = json.loads(datos_str)
+
+                                    name = data.get("name")
+                                    dpi = data.get("dpi")
+
+                                    arbolB.eliminar(name,dpi)
+                                except json.JSONDecodeError as json_error:
+                                    print(f"Error al decodificar JSON en línea: {linea}")
+                                    print(f"Mensaje de error: {json_error}")
+                                except Exception as e:
+                                    print(f"Error inesperado: {e}")
+                                pass
                 # Realizar otras operaciones o mostrar el estado final del árbol B
             except FileNotFoundError as e:
                 print("El archivo 'operaciones.json' no se encuentra.")
@@ -57,17 +125,32 @@ while not salir:
 
         else:
             print("Debe crear un objeto ArbolB primero.")
-
+        print("-----------------------------------------------------------------------")
+        arbolB.mostrar_arbol_b();
     elif opc == "2":
         # Llamar al código para buscar candidatos en el árbol B
         if arbolB is not None:
-            # Tu código para buscar candidatos en el árbol B debe ir aquí
-            pass
+
+            nombre = input("Ingrese el Nombre de la Persona: ")
+            nodo_encontrado = arbolB.find(nombre)
+
         else:
             print("Debe crear un objeto ArbolB primero.")
 
     elif opc == "3":
-        salir = True
+        # Llamar al código para buscar candidatos en el árbol B
+        if arbolB is not None:
+
+            dpi = input("Ingrese el DPI de la Persona: ")
+            nodo_encontrado = arbolB.finddpi(dpi)
+
+        else:
+            print("Debe crear un objeto ArbolB primero.")
+
+
+    elif opc == "4":
+
+
         print("Saliendo del programa.")
 
     else:
